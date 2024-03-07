6c43ed0c4d5b0ceb014cf5d9562ad9b52fc54980
WEI, ZHIXIANG <54713447+w1oves@users.noreply.github.com>
2024-03-07 23:16:17 +0800

    Update __init__.py

--- rein/models/backbones/__init__.py
+++ rein/models/backbones/__init__.py
@@ -1,6 +1,16 @@
 from .dino_v2 import DinoVisionTransformer
 from .reins_dinov2 import ReinsDinoVisionTransformer
 from .reins_eva_02 import ReinsEVA2
+from .reins_resnet import ReinsResNetV1c
+from .reins_convnext import ReinsConvNeXt
 from .clip import CLIPVisionTransformer
 
-__all__ = ["DinoVisionTransformer", "ReinsDinoVisionTransformer", "ReinsEVA2","CLIPVisionTransformer"]
+__all__ = [
+    "CLIPVisionTransformer",
+    "DinoVisionTransformer",
+    "ReinsDinoVisionTransformer",
+    "ReinsEVA2",
+    "ReinsResNetV1c",
+    "ReinsVisionTransformer",
+    "ReinsConvNeXt",
+]
